89d17bad4e7128cb615756ea386717790f15e309
HuongNTN <huongntn@payme.vn>
2023-12-24 17:10:49 +0700

    ađ hero detail

--- src/app/heros/heros.component.ts
+++ src/app/heros/heros.component.ts
@@ -3,11 +3,12 @@ import { CommonModule, NgFor } from '@angular/common';
 import { Hero } from '../interfaces/hero';
 import { FormsModule } from '@angular/forms';
 import { HEROES } from '../../mock-heroes';
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 
 @Component({
   selector: 'app-heros',
   standalone: true,
-  imports: [CommonModule, FormsModule, NgFor],
+  imports: [CommonModule, FormsModule, NgFor, HeroDetailComponent],
   templateUrl: './heros.component.html',
   styleUrl: './heros.component.css',
 })
